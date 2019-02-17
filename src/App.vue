12a1ff06aa7012ac4c97e414b70709f19bce9dd7
Daiki Kunii <dai0106ki@gmail.com>
2019-02-17 19:10:18 +0900

    init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,59 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <br>
+    <img :src="app.iconUrl" height="100" width="100">
+    <h1>{{app.name}}</h1>
+    <p>{{profile.nickname}}こんにちは！</p>
+    <button class="btn btn-primary col-xs-12 " @click="getProfile">個人情報</button>
+    <br>
+    <button class="btn btn-primary col-xs-12 " @click="getAssets">お金を見せてくれ</button>
+    <br>
+    <button class="btn btn-primary col-xs-12 " @click="donate">投げ銭</button>
+
+    <br>
+    <ul>
+      <li v-for="a in assets" :key="a.assetsId">
+        <img :src="a.iconUrl" height="15" width="15">
+        {{a.name}} - {{a.assetId}}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import coinview from '@coinjinja/coinview-sdk'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      app: {},
+      profile: {},
+      assets: [],
+    }
+  },
+  methods: {
+    async getProfile() {
+      this.profile = await coinview.user.profile()
+      alert(1)
+    },
+    async donate() {
+      const payload = {
+        assetId: '07065d64-fd33-39b5-b275-9a2cc4806ef4',
+        amount: 1,
+        description: '投げ銭'
+      }
+      const payment = await coinview.payment.create(payload)
+    },
+    async getAssets() {
+      this.assets = await coinview.user.assets()
+      alert(2)
+    }
+  },
+  async mounted() {
+    await coinview.init('7Jqg63Em')
+    this.app = await coinview.app()
   }
 }
 </script>
